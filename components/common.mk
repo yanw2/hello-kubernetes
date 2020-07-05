93d63819c6263ff81415ee14ad01736119806f55
yanw2 <wenjing.yan@anz.com>
2020-07-05 22:10:51 +1000

    Add istio resource fetch

--- components/common.mk
+++ components/common.mk
@@ -13,3 +13,14 @@ RESET  := $(shell tput -Txterm sgr0)
 define user_confirm
 	read -p "⚠️  $(1)? Continue (Y/N): " confirm && printf $$confirm | grep -iq "^[yY]" || exit 1
 endef
+
+# Check cluster to ensure that you are in the correct context when deploying
+# Usage: Makefile target, call directly
+#CURRENT_CONTEXT := $(shell kubectl config view --minify | yq r - current-context)
+#cluster.check:
+#ifeq (,$(findstring ${CLUSTER},${CURRENT_CONTEXT}))
+#	printf "❌ Project is not the right context (Cluster: $(or ${CLUSTER},${PROJECT_ID})). Aborting ...\n";
+#	exit 1
+#else
+#	printf "✅ Project is with the correct context: ${CURRENT_CONTEXT} \n"
+#endif
